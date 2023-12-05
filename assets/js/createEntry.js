43f5362e770ad21a39cc8eafcf4236ba91783d58
Matt Callaghan <mcallaghan@naperville203.org>
2023-12-05 10:59:53 -0600

    started post request

---
+++ assets/js/createEntry.js
@@ -0,0 +1,29 @@
+/**
+ * contains client-side JavaScript function
+ *  (primarily event handlers to fetch data from the Node server)
+ */
+const submitButton = document.querySelector("input.submit");
+submitButton.addEventListener("click", asynch () => {
+  // get the values entered by the user
+  const date = document.querySelector("input.date").value;
+  // a more sophisticated select that selects all input elements of class
+  //  habits that are checked
+  const habitOfMindButtons = document.querySelectorAll("input.habits:checked");
+  const habitOfMind =
+    habitOfMindButtons.length > 0 ? habitOfMindButtons[0].value : null;
+  const content = document.querySelector("textarea.content").value;
+  console.log(date + "; " + habitOfMind + "; " + content);
+
+  const entry = { date, habit: habitOfMind, content };
+
+  const response = await fetch("/createEntry", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(entry),
+  });
+
+  // for now, redirect to the home page
+  window.location = "/";
+});
